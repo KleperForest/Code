a9623d2d3d436e5e8821a4c90b76b1544f6e432c
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-05-19 14:19:20 -0600

    h

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <HardwareSerial.h>
 
+HardwareSerial mySerial(2); // Use UART2 (pins 16 and 17)
+
+void setup() {
+  Serial.begin(115200);
+  mySerial.begin(9600, SERIAL_8N1, 16, 17); // Configura la velocidad de comunicación con el HC-05
+  Serial.println("Enviando datos al Arduino Nano a través del HC-05");
 }
 
 void loop() {
-  // put your main code here, to run repea
-
+  mySerial.println("Hola desde el ESP32");
+  delay(1000); // Envía un mensaje cada segundo
 }
