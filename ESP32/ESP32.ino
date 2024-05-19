8fd7165f045be38bb9c7b801da4238b1f6a136b7
Alan Gomez <157183000+KleperForest@users.noreply.github.com>
2024-05-19 16:28:05 -0600

    S

--- ESP32/ESP32.ino
+++ ESP32/ESP32.ino
@@ -1,23 +1,14 @@
 #include "BluetoothSerial.h"
 
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
+BluetoothSerial serialBT;
+char Data;
 
-BluetoothSerial SerialBT;
-
-void setup() {
-  Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
-}
-
-void loop() {..............................................................
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
+void setup(){
+  serialBT.begin("ESP32-BT");
   }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
+
+void loop(){
+  if(serialBT.available())
+    Data = serialBT.read();
+
   }
-  delay(20);
-}
